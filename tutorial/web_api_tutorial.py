36ea187fe03cf246c9cfd4044e900d629e8f1b50
CzarDeNas <an.carde888@gmail.com>
2023-12-21 20:12:44 +0100

    3. Commit

---
+++ tutorial/web_api_tutorial.py
@@ -0,0 +1,17 @@
+'''
+Mi az az API? - Application Programming Interfaces
+
+HTTP Protocol - Web Protocol
+
+get - select or retrieve
+post - insert or create
+put - upsert : update + insert - or modify and create
+patch - egyfajta update, reszleges update - or partial modify
+delete - delete
+
+head
+conect
+options
+trace
+
+'''
